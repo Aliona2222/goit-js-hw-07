aca69cd73e5cf421f3407ea3f15a14f4a67bbaf6
Aliona2222 <aliona.prybaten@gmail.com>
2023-07-10 13:31:06 +0300

    Update_02-task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -16,34 +16,11 @@ function createGalleryItem(item) {
 const galleryMarkup = galleryItems.map(createGalleryItem).join('');
 galleryContainer.innerHTML = galleryMarkup;
 
-galleryContainer.addEventListener('click', handleGalleryClick);
-
-function handleGalleryClick(event) {
-  event.preventDefault();
-
-  if (event.target.classList.contains('gallery__image')) {
-    const largeImageUrl = event.target.dataset.source;
-    openModal(largeImageUrl);
-  }
-}
-
-function openModal(largeImageUrl) {
-    const lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-    });
-
-  const closeOnEscape = (event) => {
-    if (event.key === 'Escape') {
-        lightbox.close();
-      window.removeEventListener('keydown', closeOnEscape);
-    }
-  };
-  window.addEventListener('keydown', closeOnEscape);
-}
-
-
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
 
 
 
